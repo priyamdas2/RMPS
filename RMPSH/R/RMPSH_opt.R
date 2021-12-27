fdc1fa315e4a0d15edaf2afeccbdc671e5a66b32
Dongdong Kong <kongdd@users.noreply.github.com>
2021-12-27 10:34:34 +0800

    format `RMPSH_opt`

--- RMPSH/R/RMPSH_opt.R
+++ RMPSH/R/RMPSH_opt.R
@@ -23,23 +23,26 @@
 #' @return the optimal solution point.
 #'
 #' @examples
-#' g <- function(y)
-#' return(-20 * exp(-0.2 * sqrt(0.5 * (y[1] ^ 2 + y[2] ^ 2)))
-#'  - exp(0.5 * (cos(2 * pi * y[1]) + cos(2 * pi * y[2])))
-#'  + exp(1) + 20)
+#' g <- function(y) {
+#'   return(-20 * exp(-0.2 * sqrt(0.5 * (y[1]^2 + y[2]^2)))
+#'     - exp(0.5 * (cos(2 * pi * y[1]) + cos(2 * pi * y[2])))
+#'     + exp(1) + 20)
+#' }
 #'
-#' starting_point <- rep(1,10)
+#' starting_point <- rep(1, 10)
 #' g(starting_point)
-#' solution <- RMPSH_opt(starting_point,g, rep(-33,10), rep(33,10))
+#' solution <- RMPSH_opt(starting_point, g, rep(-33, 10), rep(33, 10))
 #' g(solution)
 #'
 #' # Will print the updates after each iteration
-#' RMPSH_opt(c(2,4,6,2,1),g,rep(-3,5), rep(23,5), verbose = 1)
+#' RMPSH_opt(c(2, 4, 6, 2, 1), g, rep(-3, 5), rep(23, 5), verbose = 1)
 #'
 #' # Will exit and return result after 2 seconds
-#' g <- function(y) { return(sum(y^2)) }
-#' RMPSH_opt(rep(2.3,100),g, rep(-11,100), rep(13,100), max_time = 2, verbose = 1)
-#'@name RMPSH_opt
+#' g <- function(y) {
+#'   return(sum(y^2))
+#' }
+#' RMPSH_opt(rep(2.3, 100), g, rep(-11, 100), rep(13, 100), max_time = 2, verbose = 1)
+#' @name RMPSH_opt
 NULL
 
 #' @rdname RMPSH_opt
@@ -51,16 +54,14 @@ RMPSH_opt <-
            ub,
            rho_1 = 2,
            rho_2 = 2,
-           phi = 10 ^ (-6),
+           phi = 1e-6,
            no_runs = 1000,
            max_iter = 10000,
            s_init = 2,
-           tol_fun = 10 ^ (-6),
-           tol_fun_2 = 10 ^ (-20),
+           tol_fun = 1e-6,
+           tol_fun_2 = 1e-20,
            max_time = 36000,
-           verbose = 0)
-  {
-
+           verbose = 0) {
     M <- length(x0)
     start_value <- func(x0)
     start_time <- Sys.time()
@@ -69,99 +70,74 @@ RMPSH_opt <-
     each_run_solution <- array(0, no_runs)
 
     ill_condition <- 0
-    for (iii in 1:no_runs)
-    {
+    for (iii in 1:no_runs) {
       epsilon <- s_init
-      if (iii == 1)
-      {
+      if (iii == 1) {
         rho <- rho_1
         theta <- anti_transformation(x0, lb, ub)
-        if(min(ub-lb) <0 )
-        {
+        if (min(ub - lb) < 0) {
           print("upper bound should be greater than lower bound")
           break
         }
         if (max(abs(theta)) > 1 ||
-            max(abs(theta)) < 0)
-        {
+          max(abs(theta)) < 0) {
           print("Starting point is outside the domain")
           break
         }
-      }
-      else{
+      } else {
         theta <- as.array(theta_array[iii - 1, ])
         rho <- rho_2
       }
-      if(ill_condition == 1)
-      {
+      if (ill_condition == 1) {
         break
       }
 
       array_of_values <- array(0, max_iter)
-      for (i in 1:max_iter)
-      {
+      for (i in 1:max_iter) {
         current_lh <- func(transformation(theta, lb, ub))
         possible_x_coords <-
           update_x(theta, epsilon, rho, phi)
         total_lh <- rep(1, 2 * M)
 
-        for (kk in 1:(2 * M))
-        {
+        for (kk in 1:(2 * M)) {
           candidate_theta <- theta
           coord_number <- ceiling(kk / 2)
-          if (possible_x_coords[kk] == theta[coord_number])
-          {
+          if (possible_x_coords[kk] == theta[coord_number]) {
             total_lh[kk] <- current_lh
-          }
-          else
-          {
+          } else {
             candidate_theta[coord_number] <- possible_x_coords[kk]
             total_lh[kk] <-
               func(transformation(candidate_theta, lb, ub))
           }
         }
         new_min <- min(total_lh)
-        if (new_min < current_lh)
-        {
+        if (new_min < current_lh) {
           position_new_min <- which.min(total_lh)
           pos_of_theta <- ceiling(position_new_min / 2)
           theta[pos_of_theta] <- possible_x_coords[position_new_min]
         }
         array_of_values[i] <- min(new_min, current_lh)
 
-        if (verbose == 1)
-        {
+        if (verbose == 1) {
           cat(
-            '\n',
-            'Run no. ',
-            iii,
-            ',iteration no.',
-            i,
-            ', current fun value = ',
-            array_of_values[i]
+            "\n", "Run no. ", iii, ",iteration no.",
+            i, ", current fun value = ", array_of_values[i]
           )
         }
 
-        if (i > 1)
-        {
-          if (abs(array_of_values[i] - array_of_values[i - 1]) < tol_fun)
-          {
-            if (abs(epsilon) > phi)
-            {
+        if (i > 1) {
+          if (abs(array_of_values[i] - array_of_values[i - 1]) < tol_fun) {
+            if (abs(epsilon) > phi) {
               epsilon <- epsilon / rho
-            }
-            else
-            {
+            } else {
               break
             }
           }
-
         }
         now_time <- Sys.time()
         now_time_spent <- as.numeric(now_time - start_time)
 
-        if (now_time_spent > max_time)
-        {
+        if (now_time_spent > max_time) {
           print("Time's up")
           paste0("Starting objective function value:  ", start_value)
           paste0("Final objective function value:  ", current_lh)
@@ -173,17 +149,13 @@ RMPSH_opt <-
       theta_array[iii, ] <- t(theta)
       each_run_solution[iii] <- func(transformation(theta, lb, ub))
 
-      if (iii > 1)
-      {
+      if (iii > 1) {
         old_soln <- theta_array[iii - 1, ]
         new_soln <- theta_array[iii, ]
-        if (norm(as.matrix(new_soln - old_soln)) < tol_fun_2)
-        {
+        if (norm(as.matrix(new_soln - old_soln)) < tol_fun_2) {
           break
         }
       }
-
-
     }
     end_time <- Sys.time()
     time_spent <- as.numeric(end_time - start_time)
