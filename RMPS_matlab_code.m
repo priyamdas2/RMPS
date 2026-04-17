b7869f6ca82996a98e9e0d919568225bb7a46ee3
Priyam Das <41349084+priyamdas2@users.noreply.github.com>
2026-04-17 15:20:51 -0400

    Update RMPS_matlab_code.m

--- RMPS_matlab_code.m
+++ RMPS_matlab_code.m
@@ -1,6 +1,6 @@
 %%%%%%%%%%%%%%%%%%%%% Citation information %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % Das, P. (2023), Black-box optimization on hyper-rectangle using Recursive Modified Pattern
-% Search and application to ROC-based Classification Problem, Indian Journal of Statistics (Sankhya B), 85, 365–404
+% Search and application to ROC-based Classification Problem, Indian Journal of Statistics (Sankhya B), 85, 365–404.
 
 
 clear all
